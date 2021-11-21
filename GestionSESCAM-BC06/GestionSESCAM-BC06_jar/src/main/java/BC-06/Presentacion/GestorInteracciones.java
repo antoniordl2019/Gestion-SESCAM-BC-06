8ebe39cb8563a812c73aafc0170dbe7e00cab6e1
JavierVillarAsensio <72606232+JavierVillarAsensio@users.noreply.github.com>
2021-11-21 10:26:42 +0100

    Create GestorInteracciones.java

---
+++ GestionSESCAM-BC06/GestionSESCAM-BC06_jar/src/main/java/BC-06/Presentacion/GestorInteracciones.java
@@ -0,0 +1,16 @@
+package Presentacion;
+
+/**
+ * Clase: Gestiona la interaccion del usuario con la app
+ * Metodos: 
+ * 		-iniciarSesion()
+ * 		-CDU1()
+ * @author javier
+ *
+ */
+
+public class GestorInteracciones {
+
+	public void iniciarSesion() {}
+	public void CDU1() {}
+}
