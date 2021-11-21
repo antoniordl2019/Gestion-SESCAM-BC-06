26e2f84890bb07a7d2692bf5f38f6c82b1aee6c4
JavierVillarAsensio <72606232+JavierVillarAsensio@users.noreply.github.com>
2021-11-21 10:24:21 +0100

    Create GestorBD.java

---
+++ GestionSESCAM-BC06/GestionSESCAM-BC06_jar/src/main/java/BC-06/Dominio/GestorBD.java
@@ -0,0 +1,23 @@
+package Dominio;
+import java.util.*;
+/**
+ * Clase: Se encarga de gestionar la Bade de Datos
+ * 
+ * Metodos:
+ * 		-ConsultarEstadoPaciente()
+ * 		-CambiarEstadoPaciente()
+ * 		-ConsultarContactosPaciente()
+ * 
+ * 
+ * @author Javier Villar Asensio
+ */
+public class GestorBD {
+
+	public String consultarEstadoPaciente(String DNI) {
+		throw new UnsupportedOperationException();
+	}
+	public void cambiarEstadoPaciente(String DNI, String nuevoEstado) {}
+	public List<String> consultarContactosPaciente(String DNI){
+		throw new UnsupportedOperationException();
+	}
+}
